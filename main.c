ebf2d39025ae5cca0679e7a05158c900cf44a463
richjones_95 <richjones_07@hotmail.co.uk>
2016-01-22 14:47:05 +0000

    Added functions for the game to run Version 1.2

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /*
 int board[25] = {
 	:,:,:,:,:,
@@ -17,28 +17,31 @@ int board[25] = {
 	:,:,:,:,:,
 }
 */
+//const int EMPTY = 0;
+//const int NOUGHTS = 1;
+//const int CROSSES = 2;
+//const int BORDER = 3;
 
-const int NOUGHTS = 1;
-const int CROSSES = 2;
-const int BORDER = 3;
-const int EMPTY = 0;
+/*enums*/
 
+enum {EMPTY, NOUGHTS, CROSSES, BORDER };
+enum {HUMAN_WIN, CPU_WIN, DRAW};
 
 int board [25];
 
 /*Function Protypes*/
 
-void Initiallise_board(int);
-void printBoard(const int);
+void initialise_board(int*);
+void print_board(const int*);
+void run_game();
 
-
-const int ConvertToGameSquares[9] = {
+const int Convert_to_game_squares[9] = {
 	6,7,8,
 	11,12,13,
 	16,17,18
 };
 
-void Initiallise_board(int *board){
+void initialise_board(int *board){
 	int i = 0; 
     for(i = 0; i < 25; ++ i){
 
@@ -47,32 +50,54 @@ void Initiallise_board(int *board){
 
     for (i = 0; i < 9; ++i){
 
-        board[ConvertToGameSquares[i]] = EMPTY;
+        board[Convert_to_game_squares[i]] = EMPTY;
     }
 
 }
 
-void printBoard (const int * board){
-    int i = 0; 
+void print_board (const int * board){
+    int i = 0;
 	printf("\nBoard:\n");
     for(i = 0; i <25; ++i){
         if(i!=0 && i % 5 == 0){
-            printf("\n");          
+            printf(" \n ");
         }
-        printf("%d", board[i]);
+        printf(" %d ", board[i]);
     }
     printf("\n");
 }
 
+void run_game(){
 
+  int game_over = 0;
+  int side = NOUGHTS;
+  int last_move_made = 0;
+  int board[25];
+
+  initialise_board(&board[0]);
+  print_board(&board[0]);
+
+  while(!game_over){
+	if(side == NOUGHTS){
+	  // Get move from human, make move on board, change side.
+
+	  }else{
+	  // Get move from humans, make move on board, change side.
+	  print_board(&board[0]);
+	  }
+	  // If three in a row exists game is over.
+
+	  // If no more moves, game is a draw.
+
+	  }
+  }
+}
 
 
 
 int main(int argc, char** argv) {
-
-    int board[25];
-    Initiallise_board(&board[0]);
-	printBoard(&board[0]);
+	srand(time(NULL));
+	run_game();
 
     return (EXIT_SUCCESS);
 }
